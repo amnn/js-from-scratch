fadf3c3ab3ee004878f157cbd1947452971bf607
Ashok Menon <amenon94@gmail.com>
2016-05-29 17:43:02 +0100

    Improving test formatting
    Putting each test in its own scope, and factoring commonly used
    variables out.

--- lambda.js
+++ lambda.js
@@ -42,32 +42,46 @@ function toBool(bool_f) {
 }
 
 /*
-assert("Encode True",  true,  toBool(fromBool(true)));
-assert("Encode False", false, toBool(fromBool(false)));
+{
+    assert("Encode True",  true,  toBool(fromBool(true)));
+    assert("Encode False", false, toBool(fromBool(false)));
+}
 */
 
 const IF  = (c, t, e) => c(t, e)();
+
 const AND = (l, r) => IF(l, () => r, () => F);
+/*
+{
+    const [t, f] = [true, false].map(fromBool);
+
+    assert("AND(F, F)", false, toBool(AND(f, f)));
+    assert("AND(F, T)", false, toBool(AND(f, t)));
+    assert("AND(T, F)", false, toBool(AND(t, f)));
+    assert("AND(T, T)", true,  toBool(AND(t, t)));
+}
+*/
+
 const OR  = (l, r) => IF(l, () => T, () => r);
-const NOT = (b)    => IF(b, () => F, () => T);
-
 /*
-assert("AND(F, F)", false, toBool(AND(fromBool(false), fromBool(false))));
-assert("AND(F, T)", false, toBool(AND(fromBool(false), fromBool(true))));
-assert("AND(T, F)", false, toBool(AND(fromBool(true),  fromBool(false))));
-assert("AND(T, T)", true,  toBool(AND(fromBool(true),  fromBool(true))));
+{
+    const [t, f] = [true, false].map(fromBool);
+
+    assert("OR(F, F)", false, toBool(OR(f, f)));
+    assert("OR(F, T)", true,  toBool(OR(f, t)));
+    assert("OR(T, F)", true,  toBool(OR(t, f)));
+    assert("OR(T, T)", true,  toBool(OR(t, t)));
+}
 */
 
+const NOT = (b) => IF(b, () => F, () => T);
 /*
-assert("OR(F, F)", false, toBool(OR(fromBool(false), fromBool(false))));
-assert("OR(F, T)", true,  toBool(OR(fromBool(false), fromBool(true))));
-assert("OR(T, F)", true,  toBool(OR(fromBool(true),  fromBool(false))));
-assert("OR(T, T)", true,  toBool(OR(fromBool(true),  fromBool(true))));
-*/
+{
+    const [t, f] = [true, false].map(fromBool);
 
-/*
-assert("NOT(T)", false, toBool(NOT(fromBool(true))));
-assert("NOT(F)", true,  toBool(NOT(fromBool(false))));
+    assert("NOT(T)", false, toBool(NOT(t)));
+    assert("NOT(F)", true,  toBool(NOT(f)));
+}
 */
 
 /** Numbers */
@@ -88,44 +102,60 @@ function toNum(num_f) {
 }
 
 /*
-assert("Encode Zero",     0, toNum(fromNum(0)));
-assert("Encode Non-Zero", 5, toNum(fromNum(5)));
+{
+    assert("Encode Zero",     0, toNum(fromNum(0)));
+    assert("Encode Non-Zero", 5, toNum(fromNum(5)));
+}
 */
 
 const IS_ZERO = (n) => n((_) => F, T);
 
 /*
-assert("Test Zero",     true,  toBool(IS_ZERO(fromNum(0))));
-assert("Test Non-Zero", false, toBool(IS_ZERO(fromNum(3))));
+{
+    assert("Test Zero",     true,  toBool(IS_ZERO(fromNum(0))));
+    assert("Test Non-Zero", false, toBool(IS_ZERO(fromNum(3))));
+}
 */
 
 const ADD  = (m, n) => (f, x) => m(f, n(f, x));
 
 /*
-assert("Add Left  Identity", 4, toNum(ADD(fromNum(0), fromNum(4))));
-assert("Add Right Identity", 4, toNum(ADD(fromNum(4), fromNum(0))));
-assert("Add Arbitrary",      5, toNum(ADD(fromNum(2), fromNum(3))));
-assert("Add Symmetric",      5, toNum(ADD(fromNum(3), fromNum(2))));
+{
+    const [n0, n2, n3, n4] = [0, 2, 3, 4].map(fromNum);
+
+    assert("Add Left  Identity", 4, toNum(ADD(n0, n4)));
+    assert("Add Right Identity", 4, toNum(ADD(n4, n0)));
+    assert("Add Arbitrary",      5, toNum(ADD(n2, n3)));
+    assert("Add Symmetric",      5, toNum(ADD(n3, n2)));
+}
 */
 
 const MUL  = (m, n) => (f, x) => m((y) => n(f, y), x);
 
 /*
-assert("Mul Left  Zero",     0, toNum(MUL(fromNum(0), fromNum(2))));
-assert("Mul Right Zero",     0, toNum(MUL(fromNum(2), fromNum(0))));
-assert("Mul Left  Identity", 4, toNum(MUL(fromNum(1), fromNum(4))));
-assert("Mul Right Identity", 4, toNum(MUL(fromNum(4), fromNum(1))));
-assert("Mul Arbitrary",      6, toNum(MUL(fromNum(2), fromNum(3))));
-assert("Mul Symmetric",      6, toNum(MUL(fromNum(3), fromNum(2))));
+{
+    const [n0, n1, n2, n3, n4] = [0, 1, 2, 3, 4].map(fromNum);
+
+    assert("Mul Left  Zero",     0, toNum(MUL(n0, n2)));
+    assert("Mul Right Zero",     0, toNum(MUL(n2, n0)));
+    assert("Mul Left  Identity", 4, toNum(MUL(n1, n4)));
+    assert("Mul Right Identity", 4, toNum(MUL(n4, n1)));
+    assert("Mul Arbitrary",      6, toNum(MUL(n2, n3)));
+    assert("Mul Symmetric",      6, toNum(MUL(n3, n2)));
+}
 */
 
 const PRED = (n) => (f, x) => n((y) => (g) => g(y(f)), CONST(x))(ID);
 const SUB  = (m, n) => n(PRED, m);
 
 /*
-assert("Sub Right Identity", 4, toNum(SUB(fromNum(4), fromNum(0))));
-assert("Sub Arbitrary",      1, toNum(SUB(fromNum(3), fromNum(2))));
-assert("Sub Saturation",     0, toNum(SUB(fromNum(2), fromNum(3))));
+{
+    const [n0, n2, n3, n4] = [0, 2, 3, 4].map(fromNum);
+
+    assert("Sub Right Identity", 4, toNum(SUB(n4, n0)));
+    assert("Sub Arbitrary",      1, toNum(SUB(n3, n2)));
+    assert("Sub Saturation",     0, toNum(SUB(n2, n3)));
+}
 */
 
 /** Pairs */
@@ -143,9 +173,12 @@ function toPair(pair) {
 }
 
 /*
-assert("Pair Encoding", [1, 2], toPair(fromPair([1, 2])));
-assert("Pair First",  1, FST(fromPair([1, 2])));
-assert("Pair Second", 2, SND(fromPair([1, 2])));
+{
+    const pair = fromPair([1, 2]);
+    assert("Pair Encoding", [1, 2], toPair(pair));
+    assert("Pair First",  1, FST(pair));
+    assert("Pair Second", 2, SND(pair));
+}
 */
 
 /** Optional */
@@ -166,8 +199,10 @@ function toOptional(opt_f) {
 }
 
 /*
-assert("Optional Null     Encoding", null, toOptional(fromOptional(null)));
-assert("Optional Non-Null Encoding", 1,    toOptional(fromOptional(1)));
+{
+    assert("Optional Null     Encoding", null, toOptional(fromOptional(null)));
+    assert("Optional Non-Null Encoding", 1,    toOptional(fromOptional(1)));
+}
 */
 
 /** Lists */
@@ -191,7 +226,9 @@ function toArray(list_f) {
 }
 
 /*
-assert("List Encoding", [1, 2, 3], toArray(fromArray([1, 2, 3])));
+{
+    assert("List Encoding", [1, 2, 3], toArray(fromArray([1, 2, 3])));
+}
 */
 
 const SPLAT = (list) => {
@@ -209,38 +246,45 @@ const HD = (l) => l((hd, _) => JUST(hd), NOTHING)
 const TL = (l) => SPLAT(l)((pair) => JUST(SND(pair)), NOTHING)
 
 /*
-assert("Empty Head",     null,   toOptional(HD(NIL)));
-assert("Non-Empty Head", 1,      toOptional(HD(CONS(1, NIL))));
-assert("Empty Tail",     null,   toOptional(TL(NIL)));
-assert("Non-Empty Tail", [2, 3], toArray(toOptional(TL(fromArray([1, 2, 3])))));
+{
+    assert("Empty Head",     null, toOptional(HD(NIL)));
+    assert("Non-Empty Head", 1,    toOptional(HD(CONS(1, NIL))));
+    assert("Empty Tail",     null, toOptional(TL(NIL)));
+
+    assert("Non-Empty Tail", [2, 3],
+           toArray(toOptional(TL(fromArray([1, 2, 3])))));
+}
 */
 
 const CONCAT = (xs, ys) => (c, n) => xs(c, ys(c, n));
 
 /*
-assert("Concat Left Identity",
-       [1, 2, 3],
-       toArray(CONCAT(NIL, fromArray([1,2,3]))));
-assert("Concat Right Identity",
-       [1, 2, 3],
-       toArray(CONCAT(fromArray([1,2,3]), NIL)));
-assert("Concat Arbitrary",
-       [1, 2, 3, 4, 5, 6],
-       toArray(CONCAT(fromArray([1,2,3]), fromArray([4, 5, 6]))));
+{
+    const xs = fromArray([1, 2, 3]);
+    const ys = fromArray([4, 5, 6]);
+
+    assert("Concat Left Identity",  [4, 5, 6],     toArray(CONCAT(NIL, ys)));
+    assert("Concat Right Identity", [1, 2, 3],     toArray(CONCAT(xs, NIL)));
+    assert("Concat Arbitrary", [1, 2, 3, 4, 5, 6], toArray(CONCAT(xs, ys)));
+}
 */
 
 const LEN = (xs) => xs((_, tailLen) => S(tailLen), Z);
 
 /*
-assert("Length Empty     List", 0, toNum(LEN(NIL)));
-assert("Length Non-Empty List", 3, toNum(LEN(fromArray([1,2,3]))));
+{
+    assert("Length Empty     List", 0, toNum(LEN(NIL)));
+    assert("Length Non-Empty List", 3, toNum(LEN(fromArray([1,2,3]))));
+}
 */
 
 const MAP = (f, xs) => xs((hd, mappedTl) => CONS(f(hd), mappedTl), NIL)
 
 /*
-assert("Map", [2, 3, 4],
-       toArray(MAP(S, fromArray([1, 2, 3].map(fromNum)))).map(toNum));
+{
+    const list = fromArray([1, 2, 3].map(fromNum));
+    assert("Map", [2, 3, 4], toArray(MAP(S, list)).map(toNum));
+}
 */
 
 const FILTER = (p, xs) => xs((hd, filteredTl) =>
@@ -250,18 +294,17 @@ const FILTER = (p, xs) => xs((hd, filteredTl) =>
                              NIL);
 
 /*
-assert("Filter", [0, 0, 0],
-       toArray(FILTER(IS_ZERO,
-                      fromArray([1, 0, 2, 0, 3, 0]
-                                  .map(fromNum))))
-         .map(toNum));
+{
+    const list = fromArray([1, 0, 2, 0, 3, 0].map(fromNum));
 
-assert("Filter Complement", [1, 2, 3],
-       toArray(FILTER(COMP(NOT, IS_ZERO),
-                      fromArray([1, 0, 2, 0, 3]
-                                  .map(fromNum))))
-         .map(toNum));
+    assert("Filter", [0, 0, 0],
+           toArray(FILTER(IS_ZERO, list)).map(toNum));
+
+    assert("Filter Complement", [1, 2, 3],
+           toArray(FILTER(COMP(NOT, IS_ZERO), list)).map(toNum));
+}
 */
+
 
 const FOLDR = (f, e, xs) => xs(f, e);
 
@@ -269,16 +312,17 @@ const FOLDL = (f, e, xs) => xs((hd, foldedTail) =>
                                (acc) => foldedTail(f(acc, hd)), ID)(e)
 
 /*
-const stringify = (x, y) => "f(" + x + ", " + y + ")";
-assert("Right fold",
-       "f(x1, f(x2, f(x3, e)))",
-       FOLDR(stringify, "e",
-             fromArray(["x1", "x2", "x3"])));
+{
+    const list = fromArray(["x1", "x2", "x3"]);
 
-assert("Left fold",
-       "f(f(f(e, x1), x2), x3)",
-       FOLDL(stringify, "e",
-             fromArray(["x1", "x2", "x3"])));
+    assert("Right fold",
+           "f(x1, f(x2, f(x3, e)))",
+           FOLDR(stringify("f"), "e", list));
+
+    assert("Left fold",
+           "f(f(f(e, x1), x2), x3)",
+           FOLDL(stringify("f"), "e", list));
+}
 */
 
 const FACT = (m) => {
@@ -290,9 +334,11 @@ const FACT = (m) => {
 }
 
 /*
-assert("0!", 1,   toNum(FACT(fromNum(0))));
-assert("1!", 1,   toNum(FACT(fromNum(1))));
-assert("5!", 120, toNum(FACT(fromNum(5))));
+{
+    assert("0!", 1,   toNum(FACT(fromNum(0))));
+    assert("1!", 1,   toNum(FACT(fromNum(1))));
+    assert("5!", 120, toNum(FACT(fromNum(5))));
+}
 */
 
 const ZIP = (xs, ys) => {
@@ -309,10 +355,17 @@ const ZIP = (xs, ys) => {
 }
 
 /*
-assert("Zip", [[1,1], [2,2], [3,3]],
-       toArray(ZIP(fromArray([1,2,3]), fromArray([1,2,3]))).map(toPair));
-assert("Zip Short Left",  [[1,1], [2,2]],
-       toArray(ZIP(fromArray([1,2]), fromArray([1,2,3]))).map(toPair));
-assert("Zip Short Right", [[1,1], [2,2]],
-       toArray(ZIP(fromArray([1,2,3]), fromArray([1,2]))).map(toPair));
+{
+    const long  = fromArray([1, 2, 3]);
+    const short = fromArray([1, 2]);
+
+    assert("Zip", [[1,1], [2,2], [3,3]],
+           toArray(ZIP(long, long)).map(toPair));
+
+    assert("Zip Short Left",  [[1,1], [2,2]],
+           toArray(ZIP(short, long)).map(toPair));
+
+    assert("Zip Short Right", [[1,1], [2,2]],
+           toArray(ZIP(long, short)).map(toPair));
+}
 */
